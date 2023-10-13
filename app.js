fae599f78b83bb62acaf413779c671b5c5b5df63
Yan Sharma <yawning008@gmail.com>
2023-10-13 10:13:13 -0400

    auysgasdfkugwAD

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 DOMSelectors = {
     nameinput : document.querySelector("#name"),
-    submit: document.querySelector("#submit"),
+    form: document.querySelector("#form"),
     h1s : document.querySelectorAll("h1"),
 
 }
 
-DOMSelectors.submit.addEventListener(
-    "click",
-    function () {
-      console.log(document.querySelector("#name").innerText); 
-    }
+DOMSelectors.form.addEventListener('submit', function(event) { 
+  event.preventDefault(); 
+
+  console.log(DOMSelectors.nameinput.value);
+
+  DOMSelectors.h1s.forEach( (h1) => (h1.textContent = DOMSelectors.nameinput.value)
   );
+
+}); 
+
+// Create a button
+/* const button = document.getElementById("myButton");
+
+// Add an event listener to the button
+button.addEventListener("click", () => {
+  // Create a card
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  // Add some text to the card
+  const text = document.createElement("p");
+  text.textContent = "This is a card";
+  card.appendChild(text);
+
+  // Append the card to the DOM
+  document.body.appendChild(card);
+}); */
+
 
 
 DOMSelectors.h1s.forEach((h1) => console.log(h1.textContent))
