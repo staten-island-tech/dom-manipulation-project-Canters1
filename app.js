0c0935c69861cad16dd7a78bf99eb71b90395a32
Yan Sharma <yawning008@gmail.com>
2023-10-16 10:15:49 -0400

    adsads

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 DOMSelectors = {
     nameinput : document.querySelector("#name"),
     imageinput : document.querySelector("#image"),
+    actorinput : document.querySelector("#actors"),
     form: document.querySelector("#form"),
     flexwrapper: document.querySelector(".flex-wrapper"),
-
+    Delete : document.querySelector("#Delete"),
+    cards : document.querySelectorAll(".card")
 }
 
 DOMSelectors.form.addEventListener('submit', function(event) { 
@@ -14,7 +16,7 @@ DOMSelectors.form.addEventListener('submit', function(event) {
   const card = document.createElement("div");
   card.classList.add("card");
 
-  const Title = document.createElement("p");
+  const Title = document.createElement("h2");
   Title.textContent = DOMSelectors.nameinput.value;
   card.appendChild(Title);
 
@@ -23,7 +25,27 @@ DOMSelectors.form.addEventListener('submit', function(event) {
   image.classList.add("image");
   card.appendChild(image);
 
+  const Starring = document.createElement("h3");
+  Starring.textContent = "Starring";
+  card.appendChild(Starring);
+
+  const Actors = document.createElement("p");
+  Actors.textContent =  DOMSelectors.actorinput.value
+  Actors.classList.add("card-sub");
+  card.appendChild(Actors);
+
+  
+
   DOMSelectors.flexwrapper.appendChild(card);
 
-}); 
+  DOMSelectors.nameinput.value =  ""
+  DOMSelectors.imageinput.value =  ""
+  DOMSelectors.actorinput.value =  ""
 
+}); 
+DOMSelectors.Delete.addEventListener('submit', function(event) { 
+  event.preventDefault(); 
+  const len = DOMSelectors.cards.len()
+  document.body.removeChild(DOMSelectors.cards[len-1]);
+  console.log(DOMSelectors.cards)
+});
