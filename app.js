75bc44b6fbe2a9db37279fe6fbd63d9dc09476e9
Yan Sharma <yawning008@gmail.com>
2023-10-20 10:13:35 -0400

    hjDXFB

--- app.js
+++ app.js
@@ -4,33 +4,51 @@ DOMSelectors = {
     actorinput : document.querySelector("#actors"),
     form: document.querySelector("#form"),
     flexwrapper: document.querySelector(".flex-wrapper"),
-    Delete : document.querySelector("#Delete"),
+    delete: document.querySelectorAll(".card-delete")
 }
 
 
 DOMSelectors.form.addEventListener('submit', function(event) { 
   event.preventDefault();
-  console.log("test");
+  
+  function makemovie() {
+    return `
+    <div class="card">
+      <h2>${DOMSelectors.nameinput.value}</h2>
+      <img src="${DOMSelectors.imageinput.value}" class="image">
+      <h3>Starring:</h3>
+      <p class="card-sub">${DOMSelectors.actorinput.value}</p>
+      <form class="card-delete">
+      <input type="submit" value="Delete" class="delbutton">
+      </form>
+    </div>`
+  };
+   const movie = makemovie();
+{/* <button class= "delbutton" onclick="this.parentNode.remove()">Delete</button> */}
   DOMSelectors.flexwrapper.insertAdjacentHTML(
     "beforeend",
-    `
-  <div class="card">
-    <h2>${DOMSelectors.nameinput.value}</h2>
-    <img src="${DOMSelectors.imageinput.value}" class="image">
-    <h3>Starring:</h3>
-    <p class="card-sub">${DOMSelectors.actorinput.value}</p>
-    <button class="delbutton" onclick="this.parentNode.remove()">Delete</button>
-  </div>
-`
+    movie
   );
 
-  DOMSelectors.nameinput.value =  "";
-  DOMSelectors.imageinput.value =  "";
-  DOMSelectors.actorinput.value =  "";
+  function clearfields() {
+    DOMSelectors.nameinput.value =  "";
+    DOMSelectors.imageinput.value =  "";
+    DOMSelectors.actorinput.value =  "";
+  }; 
+  
+  clearfields();
+  
+  console.log(DOMSelectors.delete)
 });
-DOMSelectors.Delete.addEventListener('submit', function(event) { 
+
+
+
+
+
+
+
+/* DOMSelectors.Delete.addEventListener('submit', function(event) { 
   event.preventDefault(); 
   const flexbox = DOMSelectors.flexwrapper;
   const lastcard = flexbox.lastElementChild;
-  flexbox.removeChild(lastcard);
-});
+  flexbox.removeChild(lastcard); */
