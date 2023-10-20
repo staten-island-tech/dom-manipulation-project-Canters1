078866f55fc8af50944ba8cf61f63175b128de7e
Yan Sharma <yawning008@gmail.com>
2023-10-19 20:37:02 -0400

    awdazsd

--- app.js
+++ app.js
@@ -5,46 +5,24 @@ DOMSelectors = {
     form: document.querySelector("#form"),
     flexwrapper: document.querySelector(".flex-wrapper"),
     Delete : document.querySelector("#Delete"),
-    cards : document.querySelectorAll(".card")
 }
 
+const cardHTML = `
+  <div class="card">
+    <h2>${DOMSelectors.nameinput.value}</h2>
+    <img src="${DOMSelectors.imageinput.value}" class="image">
+    <h3>Starring</h3>
+    <p class="card-sub">${DOMSelectors.actorinput.value}</p>
+    <button class="delbutton" onclick="this.parentNode.remove()">Delete</button>
+  </div>
+`;
 
 DOMSelectors.form.addEventListener('submit', function(event) { 
-  event.preventDefault(); 
-
-  console.log(DOMSelectors.nameinput.value);
-
-  const card = document.createElement("div");
-  card.classList.add("card");
-
-  const Title = document.createElement("h2");
-  Title.textContent = DOMSelectors.nameinput.value;
-  card.appendChild(Title);
-
-  const image = document.createElement("img");
-  image.src = DOMSelectors.imageinput.value;
-  image.classList.add("image");
-  card.appendChild(image);
-
-  const Starring = document.createElement("h3");
-  Starring.textContent = "Starring";
-  card.appendChild(Starring);
-
-  const Actors = document.createElement("p");
-  Actors.textContent =  DOMSelectors.actorinput.value
-  Actors.classList.add("card-sub");
-  card.appendChild(Actors);
-
-  const delbutton = document.createElement("button");
-  delbutton.classList.add("delbutton")
-  delbutton.textContent = "Delete";
-  delbutton.addEventListener('click', function(e) {
-    e.currentTarget.parentNode.remove();
-  },
-  );
-  card.appendChild(delbutton)
-
-  DOMSelectors.flexwrapper.appendChild(card);
+  event.preventDefault();
+  flexwrapper.insertadjacentHTML(
+    "beforeend",
+    cardHTML
+  )
 
   DOMSelectors.nameinput.value =  ""
   DOMSelectors.imageinput.value =  ""
